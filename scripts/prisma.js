44a5a4c77adc74e077f32d74e89e71cb7bc5d759
secondubly <secondubly@gmail.com>
2023-03-12 10:27:08 -0400

    setup prisma

---
+++ scripts/prisma.js
@@ -0,0 +1,28 @@
+'use strict'
+
+process.chdir(__dirname + '/..')
+
+const args = process.argv.slice(2)
+const nodeEnv = args.shift()
+const envs = { dev: 'development', prod: 'production' }
+
+if (!nodeEnv || !Object.keys(envs).includes(nodeEnv)) {
+	process.stderr.write('Usage: prisma [dev|prod] [prisma-commands]\n')
+	process.exit(0)
+}
+
+process.env['NODE_ENV'] = envs[nodeEnv]
+console.log('Node Env: ', process.env['NODE_ENV'])
+
+require('dotenv-flow').config({
+	path: 'src/'
+})
+
+const { spawn } = require('child_process')
+const opts = { stdio: 'inherit' }
+
+if (process.platform === 'win32') {
+	spawn('cmd', ['/c', 'node_modules\\.bin\\prisma.cmd', ...args], opts)
+} else {
+	spawn('node_modules/.bin/prisma', args, opts)
+}
