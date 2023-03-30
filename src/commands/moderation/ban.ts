7af2af2de04a132082e284f70e71ddf023284c9a
secondubly <secondubly@gmail.com>
2023-03-30 00:11:27 -0400

    update ban command

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -1,9 +1,10 @@
 import { ModerationCommand } from '#lib/moderation'
 import { ApplyOptions } from '@sapphire/decorators'
 import type { Args, Command } from '@sapphire/framework'
-import { isNullishOrEmpty } from '@sapphire/utilities'
-import type { GuildMember } from 'discord.js'
-import { Message } from 'discord.js'
+import { isNullishOrEmpty, isNullOrUndefinedOrEmpty } from '@sapphire/utilities'
+import { isTextChannel, isStageChannel } from '@sapphire/discord.js-utilities'
+import type { GuildMember, User } from 'discord.js'
+import { Message, EmbedBuilder } from 'discord.js'
 
 @ApplyOptions<ModerationCommand.Options>({
 	aliases: ['b'],
@@ -12,6 +13,15 @@ import { Message } from 'discord.js'
 	typing: true
 })
 export class UserCommand extends ModerationCommand {
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+				.addStringOption((option) => option.setName('users').setDescription('the users to ban').setRequired(true))
+				.addStringOption((option) => option.setName('reason').setDescription('Reason to ban the user(s)').setRequired(false))
+		)
+	}
 	// Message command
 	public async messageRun(message: Message, args: Args) {
 		const members = await args.repeat('member')
@@ -34,6 +44,89 @@ export class UserCommand extends ModerationCommand {
 	) {
 		const members: Promise<GuildMember[]> | GuildMember[] | undefined =
 			interactionOrMessage instanceof Message ? memberArgs : await this.parseMembers(interactionOrMessage)
+
+		if (isNullOrUndefinedOrEmpty(members)) {
+			// TODO: spit back an error message
+			if (interactionOrMessage instanceof Message) {
+				// if this was a message command, we want to send back an embed response with an error
+				const channel = interactionOrMessage.channel
+
+				if (!isTextChannel(channel) || isStageChannel(channel)) {
+					return
+				}
+
+				const errorEmbed = new EmbedBuilder()
+					.setColor(0x800000)
+					.setDescription(`${interactionOrMessage.member}, You provided invalid input, please check your input and try again.`)
+
+				channel.send({ embeds: [errorEmbed] })
+			} else {
+				// TODO: generic error response
+				interactionOrMessage.reply({ content: 'You provided invalid input, please check your input and try again.', ephemeral: true })
+			}
+			return
+		}
+
+		const guild = interactionOrMessage.guild
+		if (!guild) {
+			// TODO something went wrong, return early
+			throw Error('There was no guild object, something isn’t right.')
+		} else {
+			// TODO: Do some request batching here - maybe 10 at a time?
+			const kickPromises: Promise<string | User | GuildMember>[] = []
+			for (const member of members) {
+				kickPromises.push(guild.members.kick(member))
+			}
+
+			const kickResults = await Promise.allSettled(kickPromises).catch((err) => {
+				console.error(err)
+				console.log('some promises failed to resolve')
+				throw err
+			})
+
+			const fulfilledResponses: (string | GuildMember | User)[] = []
+			kickResults.forEach((result) => {
+				if (result.status === 'fulfilled') {
+					fulfilledResponses.push(result.value)
+				}
+			})
+
+			if (isNullOrUndefinedOrEmpty(fulfilledResponses)) {
+				// none of the users could be kicked, let the author know!
+				if (interactionOrMessage instanceof Message) {
+					const channel = interactionOrMessage.channel
+
+					if (!isTextChannel(channel) || isStageChannel(channel)) {
+						return
+					}
+
+					const errorEmbed = new EmbedBuilder()
+						.setColor(0x800000) // TODO: set this color as a constant
+						.setDescription(`${interactionOrMessage.member}, None of the supplied members could be banned.`)
+
+					channel.send({ embeds: [errorEmbed] })
+				}
+			}
+
+			const response = `Successfully kicked ${fulfilledResponses.length} of ${members.length} members.`
+			if (interactionOrMessage instanceof Message) {
+				// if this was a message command, we want to send back an embed response with an error
+				const channel = interactionOrMessage.channel
+
+				if (!isTextChannel(channel) || isStageChannel(channel)) {
+					return
+				}
+
+				const successEmbed = new EmbedBuilder().setColor(0x800000).setDescription(response)
+
+				channel.send({ embeds: [successEmbed] })
+			} else {
+				// TODO: generic error response
+				interactionOrMessage.reply({ content: response, ephemeral: true })
+			}
+
+			return
+		}
 	}
 
 	private async parseMembers(interaction: Command.ChatInputCommandInteraction | Command.ContextMenuCommandInteraction): Promise<GuildMember[]> {
@@ -49,10 +142,10 @@ export class UserCommand extends ModerationCommand {
 			}
 		} else {
 			// is a slash command
-			const membersToParse = (interaction as Command.ChatInputCommandInteraction).options.getString('members')?.split(/[\s,]+/)
+			const membersToParse = (interaction as Command.ChatInputCommandInteraction).options.getString('users')?.split(/[\s,]+/)
 
 			if (isNullishOrEmpty(membersToParse)) {
-				throw Error('No users provided.')
+				return members // return early
 			}
 
 			const guild = interaction.guild
