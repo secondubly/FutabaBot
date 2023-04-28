25ce2487a3ef5fe715852087a451f11be65c4d82
secondubly <secondubly@gmail.com>
2023-04-27 23:17:53 -0400

    connect to db before logging in

--- src/FutabaBot.ts
+++ src/FutabaBot.ts
@@ -1,10 +1,15 @@
 import './lib/setup'
 import { FutabaClient } from '#lib/FutabaClient'
+import { container } from '@sapphire/framework'
+import { PrismaClient } from '@prisma/client'
 
 const client = new FutabaClient()
 
 const main = async () => {
 	try {
+		client.logger.info('Connecting to database')
+		container.db = new PrismaClient()
+		container.db.$connect
 		client.logger.info('Logging in')
 		await client.login()
 		client.logger.info('logged in')
