dfd97e977ad80bb8444626ca0edaa190d043f319
secondubly <secondubly@gmail.com>
2023-04-29 14:30:41 -0400

    add more comments

--- src/FutabaBot.ts
+++ src/FutabaBot.ts
@@ -2,6 +2,7 @@ import './lib/setup'
 import { FutabaClient } from '#lib/FutabaClient'
 import { container } from '@sapphire/framework'
 import { PrismaClient } from '@prisma/client'
+import { SettingsManager } from '#lib/database/SettingsManager'
 
 const client = new FutabaClient()
 
@@ -9,6 +10,9 @@ const main = async () => {
 	try {
 		client.logger.info('Connecting to database')
 		container.db = new PrismaClient()
+		client.logger.info('Database connection successful')
+		container.settings = new SettingsManager()
+		client.logger.info('Settings manager initialized')
 		client.logger.info('Logging in')
 		await client.login()
 		client.logger.info('logged in')
