a8b5f1ddb922e86b99b373aea9dd0f1349e24f0e
secondubly <secondubly@gmail.com>
2023-07-23 14:45:11 -0400

    fix timeout command

--- src/commands/moderation/timeout.ts
+++ src/commands/moderation/timeout.ts
@@ -6,43 +6,37 @@ Created:  2023-07-16T14:11:34.307Z
 Modified: !date!
 */
 
-import { ModerationCommand } from "#lib/moderation";
-import { Timestamp, type FutabaCommand } from "#lib/structures";
-import { ApplyOptions } from "@sapphire/decorators";
-import type { Command } from "@sapphire/framework";
-import { ApplicationCommandType } from "discord.js";
-import { Emojis } from "#lib/constants";
-import { runAllChecks } from "#lib/util/discord/discord";
-import { Duration, DurationFormatter } from "@sapphire/duration"
-import type { TimeoutActionData } from "#lib/types/Data";
-@ApplyOptions<ModerationCommand.Options>({
-    description: 'Send a user into timeout for misbehaving with optional duration.',
-    requiredClientPermissions: ['ModerateMembers'],
-    typing: true
-})
+import { ModerationCommand } from '#lib/moderation'
+import type { FutabaCommand } from '#lib/structures'
+import { ApplyOptions } from '@sapphire/decorators'
+import type { Command } from '@sapphire/framework'
+import { ApplicationCommandType } from 'discord.js'
+import { Emojis } from '#lib/constants'
+import { runAllChecks } from '#lib/util/discord/discord'
+import { Duration, DurationFormatter } from '@sapphire/duration'
 
+@ApplyOptions<ModerationCommand.Options>({
+	description: 'Send a user into timeout for misbehaving with optional duration.',
+	requiredClientPermissions: ['ModerateMembers'],
+	typing: true
+})
 export class UserCommand extends ModerationCommand {
-    public override registerApplicationCommands(registry: Command.Registry) {
-        registry.registerChatInputCommand((builder) => 
-        builder
-            .setName(this.name)
-            .setDescription(this.description)
-            .addUserOption((option) => 
-                option
-                    .setName('user')
-                    .setDescription('Member to timeout')
-                    .setRequired(true))
-            .addStringOption((option) =>
-                option
-                    .setName('duration')
-                    .setDescription('The duration of the timeout (defaults: 60 secs)')
-                    .setRequired(false))
-            .addBooleanOption((option) =>
-                option
-                    .setName('DM')
-                    .setDescription('Send a DM to the timed out user (default: false)')
-                    .setRequired(false))
-        )
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+				.addUserOption((option) => option.setName('user').setDescription('Member to timeout').setRequired(true))
+				.addStringOption((option) =>
+					option
+						.setName('duration')
+						.setDescription('The duration of the timeout. Use 0 to remove the timeout. (default: 60 secs)')
+						.setRequired(false)
+				)
+				.addBooleanOption((option) =>
+					option.setName('dm').setDescription('Send a DM to the timed out user (default: false)').setRequired(false)
+				)
+		)
 
 		// Register Context Menu command available from any message
 		registry.registerContextMenuCommand({
@@ -55,62 +49,73 @@ export class UserCommand extends ModerationCommand {
 			name: this.name,
 			type: ApplicationCommandType.User
 		})
-    }
+	}
 
-    public override async chatInputRun(interaction: FutabaCommand.ChatInputCommandInteraction) {
-        await interaction.deferReply({ fetchReply: true });
-        const member = interaction.options.getMember('user')
-        const dm = interaction.options.getBoolean('dm') ?? false
+	public override async chatInputRun(interaction: FutabaCommand.ChatInputCommandInteraction) {
+		await interaction.deferReply({ fetchReply: true })
+		const member = interaction.options.getMember('user')
+		const dm = interaction.options.getBoolean('dm') ?? false
 
-        if (!member) {
-            return interaction.editReply({
-                content: `${Emojis.Cross} Please specify a valid member that is in this server.`
-            })
-        }
+		if (!member) {
+			return interaction.editReply({
+				content: `${Emojis.Cross} Please specify a valid member that is in this server.`
+			})
+		}
 
-        // verify that member is timeout-able
-        const { content, result } = runAllChecks(interaction.member, member, 'timeout')
-        if (!result || member.user.bot) {
-            return interaction.editReply({
-                content: content || `${Emojis.Cross} I can’t time out another bot!`
-            })
-        }
+		// verify that member is timeout-able
+		const { content, result } = runAllChecks(interaction.member, member, 'timeout')
+		if (!result || member.user.bot) {
+			return interaction.editReply({
+				content: content || `${Emojis.Cross} I can’t time out another bot!`
+			})
+		}
 
-        let time = interaction.options.getString('duration') || '60'
-        if (!isNaN(Number(time))) {
-            time += 's'
-        }
+		let time = interaction.options.getString('duration') || '60'
+		if (!isNaN(Number(time))) {
+			time += 's'
+		}
 
-        const duration = new Duration(time).offset
-        if (isNaN(duration)) {
-            return interaction.editReply({
-                content: `${Emojis.Cross} Invalid duration. Valid examples: \`1d\`, \`1h\`, \`1m\`, \`1s\``
-            })
-        }
+		const duration = new Duration(time).offset
+		if (isNaN(duration)) {
+			return interaction.editReply({
+				content: `${Emojis.Cross} Invalid duration. Valid examples: \`1d\`, \`1h\`, \`1m\`, \`1s\``
+			})
+		}
 
-        const MAX_TIMEOUT_DURATION = new Duration('28d').offset
+		const MAX_TIMEOUT_DURATION = new Duration('28d').offset
 
-        if (duration > MAX_TIMEOUT_DURATION) {
-            return interaction.editReply({
-                content: `${Emojis.Cross} Max timeout duration is 28 days.` 
-            })
-        }
+		if (duration > MAX_TIMEOUT_DURATION) {
+			return interaction.editReply({
+				content: `${Emojis.Cross} Max timeout duration is 28 days.`
+			})
+		}
 
-        const formatter = new DurationFormatter()
-        let response = `${Emojis.Confirm} ${member} has been timed out for ${formatter.format(duration)}`
+		if (duration === 0 && !member.isCommunicationDisabled()) {
+			return interaction.editReply({
+				content: `${Emojis.Cross} ${member} is not on timeout!`
+			})
+		}
 
-        const reason = interaction.options.getString('reason') ?? undefined
-        await member.timeout(duration, reason)
+		const formatter = new DurationFormatter()
+		let response = `${Emojis.Confirm} ${member} has been timed out for ${formatter.format(duration)}`
 
-        if (dm) {
-            await member.send({
-                content: `You have been timed out for ${formatter.format(duration)}.\nServer: ${interaction.guild.name}`
-            }).catch(() => {
-                response += `\n\n ${Emojis.Cross} Couldn't DM the guild member.`
-                console.warn(`Couldn't DM member ${member.id} in server ${interaction.guild.id}`)
-            })
-        }
+		const reason = interaction.options.getString('reason') ?? undefined
+		duration === 0 ? await member.timeout(null, reason) : await member.timeout(duration, reason)
 
-        return interaction.editReply(response)
-    }
+		if (duration !== 0 && dm) {
+			await member
+				.send({
+					content: `You have been timed out for ${formatter.format(duration)}.\nServer: ${interaction.guild.name}`
+				})
+				.catch(() => {
+					response += `\n\n ${Emojis.Cross} Couldn't DM the guild member.`
+					console.warn(`Couldn't DM member ${member.id} in server ${interaction.guild.id}`)
+				})
+		}
+
+		if (duration === 0) {
+			response = `${Emojis.Confirm} Removed timeout from ${member}`
+		}
+		return interaction.editReply(response)
+	}
 }
