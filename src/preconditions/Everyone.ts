2c42b77a68751a41df76b1d2e637467c3c424337
secondubly <secondubly@gmail.com>
2023-07-23 14:21:01 -0400

    messageRun is a no-op now

--- src/preconditions/Everyone.ts
+++ src/preconditions/Everyone.ts
@@ -1,8 +1,14 @@
 import { PermissionsPrecondition } from '#lib/structures'
 import { ApplyOptions } from '@sapphire/decorators'
+import type { Message } from 'discord.js'
 
 @ApplyOptions<PermissionsPrecondition.Options>({ guildOnly: false })
 export class UserPermissionsPrecondition extends PermissionsPrecondition {
+	public override async messageRun(_: Message) {
+		// no-op
+		return this.error({ message: 'This should never happen, something went wrong!' })
+	}
+
 	public handle(): PermissionsPrecondition.Result {
 		return this.ok()
 	}
