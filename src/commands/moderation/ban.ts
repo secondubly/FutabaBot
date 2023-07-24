13561e1cc73688bcab3c2ed529112fd1ef689c4e
secondubly <secondubly@gmail.com>
2023-07-24 17:46:47 -0400

    rework ban command

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -1,11 +1,12 @@
 import { ModerationCommand } from '#lib/moderation'
 import { ApplyOptions } from '@sapphire/decorators'
-import type { Args, Command } from '@sapphire/framework'
-import { isNullOrUndefinedOrEmpty } from '@sapphire/utilities'
-import { isTextChannel, isStageChannel } from '@sapphire/discord.js-utilities'
-import type { GuildMember, User } from 'discord.js'
-import { Message, EmbedBuilder, ApplicationCommandType } from 'discord.js'
-import { parseMembers } from '#utils/functions'
+import type { Command } from '@sapphire/framework'
+import type { User, APIApplicationCommandOptionChoice, Message } from 'discord.js'
+import { ApplicationCommandType, MessageContextMenuCommandInteraction } from 'discord.js'
+import type { FutabaCommand } from '#lib/structures'
+import { runAllChecks } from '#lib/util/discord/discord'
+import { Confirmation } from '#lib/structures'
+import { Emojis } from '#lib/constants'
 
 @ApplyOptions<ModerationCommand.Options>({
 	aliases: ['b'],
@@ -14,13 +15,32 @@ import { parseMembers } from '#utils/functions'
 	typing: true
 })
 export class UserCommand extends ModerationCommand {
+	private DAYS_TO_SECONDS = 86400
+	private readonly rangeChoices: APIApplicationCommandOptionChoice<Range>[] = [
+		// TODO: clean this up
+		{ name: '1 Day', value: 1 },
+		{ name: '2 Days', value: 2 },
+		{ name: '3 Days', value: 3 },
+		{ name: '4 Days', value: 4 },
+		{ name: '5 Days', value: 5 },
+		{ name: '6 Days', value: 6 },
+		{ name: '7 Days', value: 7 }
+	]
+
 	public override registerApplicationCommands(registry: Command.Registry) {
 		registry.registerChatInputCommand((builder) =>
 			builder
 				.setName(this.name)
 				.setDescription(this.description)
-				.addStringOption((option) => option.setName('users').setDescription('the users to ban').setRequired(true))
+				.addStringOption((option) => option.setName('user').setDescription('the user to ban').setRequired(true))
 				.addStringOption((option) => option.setName('reason').setDescription('Reason to ban the user(s)').setRequired(false))
+				.addIntegerOption((option) =>
+					option
+						.setName('range')
+						.setDescription('The time range of user’s message history to delete. (default: 0)')
+						.setRequired(false)
+						.setChoices(...this.rangeChoices)
+				)
 		)
 
 		// Register Context Menu command available from any message
@@ -37,58 +57,66 @@ export class UserCommand extends ModerationCommand {
 	}
 
 	// Chat Input (slash) command
-	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		return this.banUserFromInteraction(interaction)
+	public async chatInputRun(interaction: FutabaCommand.ChatInputCommandInteraction) {
+		const user = interaction.options.getUser('user', true)
+		const reason = interaction.options.getString('reason') ?? undefined
+		const range = interaction.options.getInteger('days') ?? 0
+		const dm = interaction.options.getBoolean('dm') ?? false
+
+		const { content, result } = runAllChecks(interaction.member, user, 'ban')
+		if (!result) {
+			return interaction.reply({ content, ephemeral: true })
+		}
+
+		const confirm = new Confirmation({
+			content: `Are you sure you want to ban ${user}? ${reason ? `\nReason: ${reason}` : ''}`,
+			onConfirm: async () => {
+				await this.banUser(interaction, user, reason, range, dm)
+			},
+			onCancel: ({ i }) => {
+				return i.editReply({
+					content: `${Emojis.Cross} Close call, stopped the ban hammer at the last moment!`
+				})
+			}
+		})
+		return confirm.run(interaction)
 	}
 
 	// Context Menu command
-	public async contextMenuRun(interaction: Command.ContextMenuCommandInteraction) {
-		return this.banUserFromInteraction(interaction)
+	public async contextMenuRun(interaction: FutabaCommand.ContextMenuCommandInteraction) {
+		const user = interaction.isContextMenuCommand()
+			? interaction.user
+			: (interaction as MessageContextMenuCommandInteraction<'cached'>).targetMessage.author
+		const reason = undefined
+		const range = 0
+
+		await this.banUser(interaction, user, reason, range)
+		// return this.banUser(interaction)
 	}
 
-	private async banUserFromInteraction(interaction: Command.ChatInputCommandInteraction | Command.ContextMenuCommandInteraction) {
-		const members = await parseMembers(interaction)
+	private async banUser(
+		interaction: FutabaCommand.ChatInputCommandInteraction | FutabaCommand.ContextMenuCommandInteraction,
+		user: User,
+		reason: string | undefined,
+		range: number,
+		dm = false
+	): Promise<Message> {
+		let response = `${Emojis.Confirm} ${user} has been [banned](https://tenor.com/view/11035060) ${
+			reason ? `for the following reason: __${reason}__` : ''
+		}`
 
-		if (isNullOrUndefinedOrEmpty(members)) {
-			// TODO: generic error response
-			interaction.reply({ content: 'You provided invalid input, please check your input and try again.', ephemeral: true })
-			return
+		if (dm) {
+			await user
+				.send({
+					content: `You have been banned from ${interaction.guild.name}\n${reason ? `Reason: ${reason}` : ''}`
+				})
+				.catch(() => (response += `\n\n> ${Emojis.Cross} Couldn't DM user!`))
 		}
 
-		const guild = interaction.guild
-		if (!guild) {
-			// TODO something went wrong, return early
-			throw Error('There was no guild object, something isn’t right.')
-		} else {
-			// TODO: Do some request batching here - maybe 10 at a time?
-			const kickPromises: Promise<string | User | GuildMember>[] = []
-			for (const member of members) {
-				kickPromises.push(guild.members.ban(member))
-			}
+		await interaction.guild.bans.create(user, { deleteMessageSeconds: range * this.DAYS_TO_SECONDS, reason })
 
-			const banResults = await Promise.allSettled(kickPromises).catch((err) => {
-				console.error(err)
-				console.log('some promises failed to resolve')
-				throw err
-			})
-
-			const fulfilledResponses: (string | GuildMember | User)[] = []
-			banResults.forEach((result) => {
-				if (result.status === 'fulfilled') {
-					fulfilledResponses.push(result.value)
-				}
-			})
-
-			if (isNullOrUndefinedOrEmpty(fulfilledResponses)) {
-				// none of the users could be kicked, let the author know!
-				const response = 'None of the supplied authors could be banned.'
-				interaction.reply({ content: response, ephemeral: true })
-				return
-			}
-
-			const response = `Successfully kicked ${fulfilledResponses.length} out of ${members.length} members.`
-			interaction.reply({ content: response, ephemeral: true })
-			return
-		}
+		return interaction.editReply(response)
 	}
 }
+
+type Range = 1 | 2 | 3 | 4 | 5 | 6 | 7
