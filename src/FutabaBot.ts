04eac9ed514023724e56e11c6c6fb3b0ece766e4
secondubly <secondubly@gmail.com>
2023-08-30 19:33:31 -0400

    update ts-config to newest version for typescript 5.0 support

--- src/FutabaBot.ts
+++ src/FutabaBot.ts
@@ -1,4 +1,4 @@
-import './lib/setup'
+import '#lib/setup'
 import { FutabaClient } from '#lib/FutabaClient'
 import { container } from '@sapphire/framework'
 import { PrismaClient } from '@prisma/client'
