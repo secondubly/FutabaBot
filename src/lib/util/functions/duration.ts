2cf44f50b59fe6cd769a708b44a8e0c72a9764d8
secondubly <secondubly@gmail.com>
2023-09-25 15:40:56 -0400

    add seconds utility function

--- src/lib/util/functions/duration.ts
+++ src/lib/util/functions/duration.ts
@@ -4,3 +4,8 @@ export function mins(minutes: number) {
     if (isNaN(minutes)) throw new Error('Input must be a valid number');
     return Time.Minute * minutes
 }
+
+export function sec(seconds: number) {
+    if (isNaN(seconds)) throw new Error('Input must be a valid number');
+    return Time.Second * seconds
+}
